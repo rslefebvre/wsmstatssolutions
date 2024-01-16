a94d2fb7960f74c4c8a12804957b77e79ced06d4
rslefebvre <50893366+rslefebvre@users.noreply.github.com>
2024-01-16 06:05:03 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,167 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "   Ricky Lefebvre         "
+},
+{
+  "id": "section-01-01",
+  "level": "1",
+  "url": "section-01-01.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Introduction",
+  "body": " Introduction  Text of section.  "
+},
+{
+  "id": "section-01-02",
+  "level": "1",
+  "url": "section-01-02.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Characterizing a Set of Measurements: Graphical Methods",
+  "body": " Characterizing a Set of Measurements: Graphical Methods  Text of section.  "
+},
+{
+  "id": "section-01-03",
+  "level": "1",
+  "url": "section-01-03.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Characterizing a Set of Measurements: Numerical Methods",
+  "body": " Characterizing a Set of Measurements: Numerical Methods  Text of section.  "
+},
+{
+  "id": "section-01-04",
+  "level": "1",
+  "url": "section-01-04.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "How Inferences Are Made",
+  "body": " How Inferences Are Made  Text of section.  "
+},
+{
+  "id": "section-01-05",
+  "level": "1",
+  "url": "section-01-05.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Theory and Reality",
+  "body": " Theory and Reality  Text of section.  "
+},
+{
+  "id": "section-01-06",
+  "level": "1",
+  "url": "section-01-06.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "Summary",
+  "body": " Summary  Text of section.  "
+},
+{
+  "id": "section-01-SE",
+  "level": "1",
+  "url": "section-01-SE.html",
+  "type": "Section",
+  "number": "1.7",
+  "title": "Supplementary Exercises",
+  "body": " Supplementary Exercises  Text of section.  "
+},
+{
+  "id": "section-02-01",
+  "level": "1",
+  "url": "section-02-01.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Introduction",
+  "body": " Introduction  Text of section.  "
+},
+{
+  "id": "section-03-01",
+  "level": "1",
+  "url": "section-03-01.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Basic Definition",
+  "body": " Basic Definition  Text of section.  "
+},
+{
+  "id": "section-04-01",
+  "level": "1",
+  "url": "section-04-01.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Introduction",
+  "body": " Introduction  Text of section.  "
+},
+{
+  "id": "section-05-01",
+  "level": "1",
+  "url": "section-05-01.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Introduction",
+  "body": " Introduction  Text of section.  "
+},
+{
+  "id": "section-06-01",
+  "level": "1",
+  "url": "section-06-01.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Introduction",
+  "body": " Introduction  A measure of the goodness of an estimate is the error of estimation, the difference between the estimate and the parameter estimated (for example, the difference between and ).  We will assume throughout the remainder of this text that populations are large in comparison to the sample size and consequently that the random variables obtained through a random sample are in fact independent of one another.  "
+},
+{
+  "id": "section-06-02",
+  "level": "1",
+  "url": "section-06-02.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Finding the Probability Distribution of a Function of Random Variables",
+  "body": " Finding the Probability Distribution of a Function of Random Variables  Text of section.  "
+},
+{
+  "id": "section-06-03",
+  "level": "1",
+  "url": "section-06-03.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "The Method of Distribution Functions",
+  "body": " The Method of Distribution Functions  Text of section.    Let be a random variable with probability density function given by    Find the density function of .   Find the density function of .   Find the density function of .   Find , , and by using the derived density functions for these random variables.   Find , , and by the methods of Chapter 4.     "
+},
+{
+  "id": "exercises-06-03-1",
+  "level": "2",
+  "url": "section-06-03.html#exercises-06-03-1",
+  "type": "Exercise",
+  "number": "6.3.6.1",
+  "title": "",
+  "body": " Let be a random variable with probability density function given by    Find the density function of .   Find the density function of .   Find the density function of .   Find , , and by using the derived density functions for these random variables.   Find , , and by the methods of Chapter 4.  "
+},
+{
+  "id": "exercises-06-03-2",
+  "level": "2",
+  "url": "section-06-03.html#exercises-06-03-2",
+  "type": "Exercise",
+  "number": "6.3.6.2",
+  "title": "",
+  "body": ""
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
